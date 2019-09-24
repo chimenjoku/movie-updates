4356c949da15d2c3657e5c9d93dfcf3dc461d613
Chime Njoku <chimeenjoku@gmail.com>
2019-09-24 17:50:09 -0500

    initial commit

---
+++ backend/src/main/java/com/example/MovieUpdates/MovieUpdatesApplication.java
@@ -0,0 +1,13 @@
+package com.example.MovieUpdates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieUpdatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieUpdatesApplication.class, args);
+	}
+
+}
