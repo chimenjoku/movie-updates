e2e4b8f7736c38fcf1f43462193e1574dc39f3fb
Chime Njoku <chimeenjoku@gmail.com>
2019-10-02 15:23:04 -0500

    added routes for upcoming and in theatres movies

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,25 +2,43 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { MoviesListComponent } from './components/movies-list/movies-list.component';
 import { UpcomingMoviesListComponent } from './components/upcoming-movies-list/upcoming-movies-list.component';
 import { NowPlayingMoviesListComponent } from './components/now-playing-movies-list/now-playing-movies-list.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { Router, RouterModule, Routes } from '@angular/router';
+import { MoviesService } from './movies.service';
 
+const appRoutes: Routes = [
+  {
+    path: 'upcoming-movies',
+    component: UpcomingMoviesListComponent
+  },
+  {
+    path: 'in-theatres',
+    component: NowPlayingMoviesListComponent
+  },
+  {
+    path: '',
+    component: UpcomingMoviesListComponent,
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    MoviesListComponent,
     UpcomingMoviesListComponent,
     NowPlayingMoviesListComponent,
     NavigationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
-  providers: [],
+  providers: [MoviesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
