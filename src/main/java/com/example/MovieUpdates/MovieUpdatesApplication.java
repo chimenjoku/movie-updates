891e34d5e4f7d6243f88e304e29da1d68def645b
Chime Njoku <chimeenjoku@gmail.com>
2019-12-23 14:54:40 -0600

    maven pom.xml changes

--- src/main/java/com/example/MovieUpdates/MovieUpdatesApplication.java
+++ src/main/java/com/example/MovieUpdates/MovieUpdatesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class MovieUpdatesApplication {
+public class MovieUpdatesApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(MovieUpdatesApplication.class);
 	@Bean
